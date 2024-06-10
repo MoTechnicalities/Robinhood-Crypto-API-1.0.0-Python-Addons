17428ec445317823502d429cb5ccdc643c5b12fc
MoTechnicalities <68468472+MoTechnicalities@users.noreply.github.com>
2024-06-10 12:19:11 -0400

    Update robinhood_api_account_info_sample.py

--- robinhood_api_account_info_sample.py
+++ robinhood_api_account_info_sample.py
@@ -21,8 +21,23 @@ def main():
     code Loops, the Wait Time in secconds befor the next Loop, and The Buy and Sell
     Threshold. Adjust the setting to fit your needs.
     """
-# SET ITERATIONS
-    num_iterations = 4
+
+
+# VARIABLES for TRADING
+    # Set the number of times the Loop will iterate
+    num_iterations = 10000
+    # Specify sleep_time in seconds to wait befor next trade attempt.
+    sleep_time = (5 * 60)
+
+    # Specify Buy and Sell Trading Thresholds Exampe: 5.00
+    threshold_buy = 5.00
+    threshold_sell = 5.00
+
+    # Specify if ACTUAL REAL LIVE Buy/Sell ("YES")
+    live_buy = "YES"
+    live_sell = "YES"
+
+#Begin the Loops    
     for i in range(num_iterations):
         
 
@@ -54,11 +69,11 @@ def main():
         eth_data = eth_bid_askings['results'][0]
         try:
           eth_ask = float(eth_data['ask_inclusive_of_buy_spread'])
-          #eth_ask = (3276.15) # uncomment to inject fake price eth_ask = (3716.0185916)
+        #  eth_ask = (5276.15) # uncomment to inject fake price eth_ask = (3716.0185916)
           eth_price = float(eth_data['price'])
-          #eth_price = (3276.15) # uncomment to inject fake price eth_price = (3699.3992958)
+        #  eth_price = (5276.15) # uncomment to inject fake price eth_price = (3699.3992958)
           eth_bid = float(eth_data['bid_inclusive_of_sell_spread'])
-          #eth_bid = (3276.15) # uncomment to inject fake price eth_bid = (3682.78)
+        #  eth_bid = (5276.15) # uncomment to inject fake price eth_bid = (3682.78)
         except ValueError:
           print("Price values are not numbers")  # Handle non-numeric values
         print(f"Ask Price: {eth_ask}")
@@ -73,11 +88,8 @@ def main():
         print("USD Bid Value of ETH: ", eth_usd_val_bid)
 
 
-# Check for Buy/Sell Opportunities Using the Retrieved Data
+# Check for Buy/Sell Opportunities using retrieved data
 
-# Specify Buy and Sell Thresholds
-        threshold_buy = 5.00
-        threshold_sell = 5.00
 # Buy Action
         buy_amount = (buying_power - eth_usd_val_ask) * 0.5   # Calculate 50% of the difference
         print("Checking Opportunities to Buy....")
@@ -94,30 +106,28 @@ def main():
             # Place the order with the calculated asset_quantity
             formatted_quantity = f"{eth_to_buy:.6f}"
 
-            # !!! THE BELOW STEP DOES THE ACTUAL BUY - REMOVE """ TO FUNCTION !!!
-            """
-            order = api_trading_client.place_order(
-                str(uuid.uuid4()),
-                "buy",
-                "market",
-                "ETH-USD",
-                {"asset_quantity": formatted_quantity}  # Convert to string for the API
-            )
-            """
-            # !!! THE ABOVE STEP DOES THE ACTUAL BUY - REMOVE """ TO FUNCTION !!!
+            if live_buy == "YES":
+                print("LIVE BUY TRADING")
+                order = api_trading_client.place_order(
+                    str(uuid.uuid4()),
+                    "buy",
+                    "market",
+                    "ETH-USD",
+                    {"asset_quantity": formatted_quantity}  # Convert to string for the API
+                )
+            else:
+                print("SIMULATED BUY TRADING")
 
             # Print confirmation message with the amount of ETH bought
             print(f"Bought {eth_to_buy:.6f} ETH for approximately ${buy_amount:.2f}")
 
 
 # Sell Action
-    #Set a Buy/Sell threshold
         sell_amount = (eth_usd_val_bid - buying_power) * 0.5  # Calculate 50% of the difference
         print("Checking Opportunities to Sell...")
         if sell_amount > threshold_sell:
             print(f"I need to Sell: ${sell_amount:.2f}")
-            # Assuming you have already retrieved the ETH price (eth_price) and total ETH quantity (total_quantity)
-
+    
             # Calculate the number of ETH units to sell based on sell_amount and current price
             eth_to_sell = float(sell_amount) / float(eth_price)
 
@@ -128,17 +138,17 @@ def main():
             
             formatted_quantity = f"{eth_to_sell:.6f}"
 
-            # !!! THE BELOW STEP DOES THE ACTUAL SELL - REMOVE """ TO FUNCTION !!!
-            """
-            order = api_trading_client.place_order(
-                str(uuid.uuid4()),
-                "sell",
-                "market",
-                "ETH-USD",
-                {"asset_quantity": formatted_quantity}  # Convert to string for the API
-            )
-            """
-            # !!! THE ABOVE STEP DOES THE ACTUAL SELL - REMOVE """ TO FUNCTION !!!
+            if live_sell == "YES":
+                print("LIVE SELL TRADING")
+                order = api_trading_client.place_order(
+                    str(uuid.uuid4()),
+                    "sell",
+                    "market",
+                    "ETH-USD",
+                    {"asset_quantity": formatted_quantity}  # Convert to string for the API
+                )
+            else:
+                print("SIMULATED SELL TRADING")
 
             print(str(eth_to_sell))
             print(f"Sold {eth_to_sell:.6f} ETH for approximately ${sell_amount:.2f}")
@@ -147,26 +157,12 @@ def main():
 
 # Sleep for the specified time before each iteration (except the last iteration).
         if i < num_iterations - 1:  # Check if not the last iteration
-            sleep_time = (5 * 60)   # Specify sleep_time in seconds.
             print(f"Waiting for {sleep_time / 60} minutes before next Iteration...")
             time.sleep(sleep_time)  # Sleep
 # Do this after all iterations have compleated.
         else:
-            print("All loop completed. Ending session.")
+            print("All iterations completed. Ending session.")
             print("")
-
-
-        """
-        BUILD YOUR TRADING STRATEGY HERE
-
-        order = api_trading_client.place_order(
-            str(uuid.uuid4()),
-            "buy",
-            "market",
-            "BTC-USD",
-            {"asset_quantity": "0.0001"}
-        )
-        """
 
 
 if __name__ == "__main__":
